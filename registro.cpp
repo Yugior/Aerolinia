12d232fe310c5e938d2f634fb8aa875efac101e0
Yugior <150100500+Yugior@users.noreply.github.com>
2023-11-29 17:48:50 -0600

    Update registro.cpp

--- registro.cpp
+++ registro.cpp
@@ -1,49 +1,46 @@
-#include <iostream>
+// registro.cpp
 #include "aerolinea.h"
-#include "vuelo.h"
-#include "pasajero.h"
-#include "costo.h"
-#include "comercial.h"
-#include "trabajo.h"
-#include "premier.h"
 
 int main() {
-    // Crear objetos
-    Aerolinea aerolinea;
-    Vuelo vuelo;
-    Pasajero pasajero;
-    Costo costo;
-    Comercial comercial;
-    Trabajo trabajo;
-    Premier premier;
+        
+        char deseaNuevoRegistro = 's'; // Inicializar con 's' para entrar al bucle
 
-    // Configurar la aerolínea
-    aerolinea.setVuelo(vuelo);
-    aerolinea.setPasajero(pasajero);
-    aerolinea.getPasajero().setCosto(costo);
-    aerolinea.getPasajero().getCosto().setComercial(comercial);
-    aerolinea.getPasajero().getCosto().setTrabajo(trabajo);
-    aerolinea.getPasajero().getCosto().getTrabajo().setPremier(premier);
-
-    // Pedir información
-    aerolinea.pedirInformacion();
-
-    // Imprimir las características del vuelo
-    std::cout << "\n--- Características del Vuelo ---\n";
-    std::cout << "Pasajero: " << aerolinea.getPasajero().getNombre() << "\n";
-    std::cout << "Tipo de Clase: " << aerolinea.getPasajero().getCosto().getLugarAsiento() << "\n";
-    
-    if (aerolinea.getPasajero().getCosto().getLugarAsiento() == "Comercial") {
-        std::cout << "Número de Asientos (Clase Comercial): " << aerolinea.getPasajero().getCosto().getComercial().getNumAsientos() << "\n";
-    } else if (aerolinea.getPasajero().getCosto().getLugarAsiento() == "Trabajo") {
-        std::cout << "Número de Asientos (Clase Trabajo): " << aerolinea.getPasajero().getCosto().getTrabajo().getNumAsientos() << "\n";
-        std::cout << "Tipo de Servicio (Clase Trabajo): " << aerolinea.getPasajero().getCosto().getTrabajo().getTipoServicio() << "\n";
-    } else if (aerolinea.getPasajero().getCosto().getLugarAsiento() == "Premier") {
-        std::cout << "Número de Asientos (Clase Premier): " << aerolinea.getPasajero().getCosto().getTrabajo().getPremier().getNumAsientos() << "\n";
-        std::cout << "Servicio 1 (Clase Premier): " << aerolinea.getPasajero().getCosto().getTrabajo().getPremier().getServicio1() << "\n";
-        std::cout << "Servicio 2 (Clase Premier): " << aerolinea.getPasajero().getCosto().getTrabajo().getPremier().getServicio2() << "\n";
-        std::cout << "Servicio 3 (Clase Premier): " << aerolinea.getPasajero().getCosto().getTrabajo().getPremier().getServicio3() << "\n";
-    }
+        while (deseaNuevoRegistro == 's' || deseaNuevoRegistro == 'S') {
+        
+            // Crear instancias de las clases
+            Aerolinea aerolinea;
+        
+            // Seleccionar la aerolínea
+            aerolinea.seleccionaAerolinea();
+        
+            // Seleccionar la fecha del vuelo
+            aerolinea.getVuelo().seleccionaVuelo();
+        
+            // Registrar al pasajero
+            Pasajero pasajero;
+            pasajero.registrarPasajero();
+            aerolinea.setPasajero(pasajero);
+        
+            // Elegir la clase del asiento
+            aerolinea.getVuelo().eligeClase();
+        
+            // Imprimir la información registrada
+            std::cout << "\n--- Resumen de Registro ---\n";
+            std::cout << "Aerolínea: " << aerolinea.getAerolinea() << "\n";
+            std::cout << "Fecha del vuelo: " << aerolinea.getVuelo().getFecha() << "\n";
+            std::cout << "Nombre del pasajero: " << aerolinea.getPasajero().getNombre() << "\n";
+            std::cout << "Teléfono del pasajero: " << aerolinea.getPasajero().getTelefono() << "\n";
+            std::cout << "Asiento: " << (aerolinea.getVuelo().getClase() == 1 ? "Trabajo" : "Premier") << "\n";
+            
+            // Imprimir el costo del asiento
+            std::cout << "Costo del asiento: $" << aerolinea.getVuelo().getCosto() << "\n";
+            
+            // Preguntar si desea un nuevo registro
+            std::cout << "¿Desea realizar un nuevo registro? (s/n): ";
+            std::cin >> deseaNuevoRegistro;
+        }
+        
+        std::cout << "Programa finalizado.\n";
 
     return 0;
 }
