749efec070cc778e54c90d1791c1035834afb72a
Yugior <150100500+Yugior@users.noreply.github.com>
2023-11-29 17:51:22 -0600

    Update vuelo.cpp

--- vuelo.cpp
+++ vuelo.cpp
@@ -1,27 +1,97 @@
+// vuelo.cpp
 #include "vuelo.h"
-#include <iostream>
 
-// Constructor
-Vuelo::Vuelo() {}
-
-// Getter y Setter para Piloto
-std::string Vuelo::getPiloto() const {
-    return piloto;
+Vuelo::Vuelo() {
+    // Constructor por defecto
 }
 
-void Vuelo::setPiloto(const std::string& piloto) {
-    this->piloto = piloto;
+// Getters
+std::string Vuelo::getFecha() const {
+    return fecha;
 }
 
-// Getter y Setter para Copiloto
-std::string Vuelo::getCopiloto() const {
-    return copiloto;
+std::string Vuelo::getAvion() const {
+    return avion;
 }
 
-void Vuelo::setCopiloto(const std::string& copiloto) {
-    this->copiloto = copiloto;
+int Vuelo::getClase() const {
+    return clase;
 }
 
-// Getter y Setter para Fecha de salida
-std::string Vuelo::getFechaSalida() const {
-    return fechaSalida;
+double Vuelo::getCosto() const {
+    return costo;
+}
+
+// Setters
+void Vuelo::setFecha(const std::string &fecha) {
+    this->fecha = fecha;
+}
+
+void Vuelo::setAvion(const std::string &avion) {
+    this->avion = avion;
+}
+
+void Vuelo::setClase(int clase) {
+    this->clase = clase;
+}
+
+void Vuelo::setCosto(double costo) {
+    this->costo = costo;
+}
+
+// Métodos adicionales
+void Vuelo::seleccionaVuelo() {
+    std::cout << "Selecciona la fecha del vuelo:\n";
+    std::cout << "1. Lunes\n";
+    std::cout << "2. Martes\n";
+    std::cout << "3. Miércoles\n";
+    std::cout << "4. Jueves\n";
+    std::cout << "5. Viernes\n";
+
+    int opcion;
+    std::cin >> opcion;
+
+    switch (opcion) {
+        case 1:
+            setFecha("Lunes");
+            break;
+        case 2:
+            setFecha("Martes");
+            break;
+        case 3:
+            setFecha("Miércoles");
+            break;
+        case 4:
+            setFecha("Jueves");
+            break;
+        case 5:
+            setFecha("Viernes");
+            break;
+        default:
+            std::cout << "Opción no válida.\n";
+            break;
+    }
+}
+
+void Vuelo::eligeClase() {
+    std::cout << "Elige la clase del asiento:\n";
+    std::cout << "1. Trabajo\n";
+    std::cout << "2. Premier\n";
+
+    int opcion;
+    std::cin >> opcion;
+
+    switch (opcion) {
+        case 1:
+            setClase(1);  // Trabajo
+            setCosto(50.0);  // Establecer costo para la clase Trabajo
+            break;
+        case 2:
+            setClase(2);  // Premier
+            setCosto(100.0);  // Establecer costo para la clase Premier
+            break;
+        default:
+            std::cout << "Opción no válida.\n";
+            break;
+    }
+}
